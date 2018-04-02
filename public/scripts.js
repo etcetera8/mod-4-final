96ab60a9a579d51d1368065c28e0abb06f4be7ad
parker lindley <fugazi8@gmail.com>
2018-04-02 10:10:51 -0600

    Display items list with correct checks

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,27 @@
 const itemInput = $('.item-input');
 const button = $('.submit-btn');
 
+$(document).ready( async () => {
+  console.log('ready');
+  const response = await fetch('/api/v1/items')
+  const items = await response.json();
+  console.log(items);
+
+  items.forEach( item => {
+    let checked=  item.packed ? 
+      `<input class="checkbox "type="checkbox" value="packed" checked>`:
+      `<input class="checkbox "type="checkbox" value="packed">`
+    
+    const template = `<article class="card">
+                        <h3>${item.item}</h3>
+                        <button class="killme">Delete</button>
+                        ${checked}
+                      </article>`
+    $('main').prepend(template)
+  })
+
+})
+
 button.click(() => addItem(event))
 
 const addItem = (event) => {
