efc8a27810330f638637f2543c264e946f5ce0c4
parker lindley <fugazi8@gmail.com>
2018-04-02 11:14:11 -0600

    Deleting from backend

--- public/scripts.js
+++ public/scripts.js
@@ -6,13 +6,14 @@ $(document).ready( async () => {
   const items = await response.json();
 
   items.forEach( item => {
+    console.log(item);
     let checked =  item.packed ? 
       `<span><input class="checkbox "type="checkbox" value="packed" checked> <label>packed</label></span>`:
       `<span><input class="checkbox "type="checkbox" value="packed"> <label>packed</label></span>`
     
     const template = `<article class="card">
                         <h3>${item.item}</h3>
-                        <button class="killme">Delete</button>
+                        <button class="killme" value=${item.id} onClick=deleteItem(${item.id})>Delete</button>
                         ${checked}
                       </article>`
     $('main').prepend(template)
@@ -21,6 +22,17 @@ $(document).ready( async () => {
 })
 
 button.click(() => addItem(event))
+
+const deleteItem = (itemId) => {
+  fetch(`/api/v1/items/${itemId}`, {
+    method: 'DELETE'
+  }).then(response => { 
+    return response.json()
+  })
+  .catch(error => {
+    console.log("error", error);
+  })
+}
 
 const addItem = (event) => {
   event.preventDefault();
@@ -48,7 +60,7 @@ const postItem = (item) => {
     return response.json()
   })
   .then ( results => {
-    console.log("these are the results", results)
+    console.log("these are the results", results.item[0])
   })
   .catch( error => {
     console.log('request failed', error);
