eb36ca0f3c77d1cdd1ffad1e35350b44fd69dce7
parker lindley <fugazi8@gmail.com>
2018-04-02 09:58:34 -0600

    Capture input value

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,15 @@
-console.log("hi");
+const itemInput = $('.item-input');
+const button = $('.submit-btn');
+
+button.click(() => addItem(event))
+
+const addItem = (event) => {
+  event.preventDefault();
+  const item = itemInput.val();
+  if (item === '') {
+    console.log('No empty items')
+  } else {
+    console.log(item)
+  }
+  itemInput.val('')
+}
