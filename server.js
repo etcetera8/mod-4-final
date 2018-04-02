17487d44f1c8e0f90f2fb885aa018c840e8b98b9
parker lindley <fugazi8@gmail.com>
2018-04-02 09:47:04 -0600

    Write first get endpoint

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-
-})
-
+app.get('/', (request, response) => {})
 app.set('port', process.env.PORT || 3000);
+
+app.get('/api/v1/items', (request, response) => {
+  database('list').select()
+    .then( items => {
+      response.status(200).json(items);
+    })
+    .catch( error => {
+      response.status(500).json({ error });
+    })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Server is running on ${app.get('port')}`)
