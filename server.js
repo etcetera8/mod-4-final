04b6f061a53f084a52f608c6e0aafc88434eafd0
parker lindley <fugazi8@gmail.com>
2018-04-02 13:54:41 -0600

    Update for prod

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const environment = process.env.NODE_ENV || 'production';
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.get('/', (request, response) => {})
 app.set('port', process.env.PORT || 3000);
 
 app.get('/api/v1/items', (request, response) => {
